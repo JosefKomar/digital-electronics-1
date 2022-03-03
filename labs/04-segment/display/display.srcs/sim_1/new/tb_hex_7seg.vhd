c286ea277bb71c824beaf48299e26ebca71d2eae
student <student@PC-630.urel.feec.vutbr.cz>
2022-03-03 12:49:53 +0100

    cv04

---
+++ labs/04-segment/display/display.srcs/sim_1/new/tb_hex_7seg.vhd
@@ -0,0 +1,73 @@
+------------------------------------------------------------
+--
+-- Testbench for 7-segment display decoder.
+-- Nexys A7-50T, Vivado v2020.1, EDA Playground
+--
+-- Copyright (c) 2020-Present Tomas Fryza
+-- Dept. of Radio Electronics, Brno Univ. of Technology, Czechia
+-- This work is licensed under the terms of the MIT license.
+--
+------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+------------------------------------------------------------
+-- Entity declaration for testbench
+------------------------------------------------------------
+entity tb_hex_7seg is
+    -- Entity of testbench is always empty
+end entity tb_hex_7seg;
+
+------------------------------------------------------------
+-- Architecture body for testbench
+------------------------------------------------------------
+architecture testbench of tb_hex_7seg is
+
+    -- Local signals
+    signal s_hex  : std_logic_vector(4 - 1 downto 0);
+    signal s_seg  : std_logic_vector(7 - 1 downto 0);
+
+begin
+    -- Connecting testbench signals with decoder entity
+    -- (Unit Under Test)
+    uut_hex_7seg : entity work.hex_7seg
+        port map(
+            hex_i => s_hex,
+            seg_o => s_seg
+        );
+
+    --------------------------------------------------------
+    -- Data generation process
+    --------------------------------------------------------
+    p_stimulus : process
+    begin
+        report "Stimulus process started" severity note;
+
+        -- First test case
+        s_hex <= "0000"; wait for 50 ns;
+        assert (s_seg = "0000001")
+        report "Input combination 0000 (0) FAILED" severity error;
+
+        -- WRITE OTHER TEST CASES HERE
+        s_hex <= "0001"; wait for 50 ns;
+        assert (s_seg = "1001111")
+        report "Input combination 0000 (1) FAILED" severity error;
+        
+        s_hex <= "0100"; wait for 50 ns;
+        assert (s_seg = "1001100")
+        report "Input combination 0100 (4) FAILED" severity error;
+        
+        s_hex <= "1100"; wait for 50 ns;
+        assert (s_seg = "0110001")
+        report "Input combination 1100 (C) FAILED" severity error;
+        
+        s_hex <= "1111"; wait for 50 ns;
+        assert (s_seg = "0111000")
+        report "Input combination 1100 (F) FAILED" severity error;
+
+        report "Stimulus process finished" severity note;
+        wait;
+    end process p_stimulus;
+
+end architecture testbench;
